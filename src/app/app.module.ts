d0e63807d6b1f5334767ea8583a0a3187806cda0
unknown <viniciusfa.delima@gmail.com>
2023-10-22 21:16:03 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './component/menu-bar/menu-bar.component';
+import { CardComponent } from './component/card/card.component';
+import { CardLabelComponent } from './component/card/card-label/card-label.component';
+import { CardPricingComponent } from './component/card/card-pricing/card-pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuBarComponent,
+    CardComponent,
+    CardLabelComponent,
+    CardPricingComponent
   ],
   imports: [
     BrowserModule,
